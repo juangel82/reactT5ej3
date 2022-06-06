c6d627e91180a1877e0945c9124a25ee644d73f2
juangel82 <juanillo82@gmail.com>
2022-06-06 22:43:43 +0200

    se añade el blqoeuo

--- src/components/MouseColor.jsx
+++ src/components/MouseColor.jsx
@@ -3,17 +3,32 @@ import React,{useState} from 'react';
 
 const MouseColor = () => {
     const [color, setColor] = useState('gris');
+    const [bloqueo,setBloqueo]=useState(0);
     function capturarRaton(){
         let colores =['azul','gris','verde','rojo'];
         let aleatorio = Math.floor(Math.random() * (4));
-        console.log(aleatorio);
+        //console.log(aleatorio);
+        if (bloqueo ===1){
         setColor(colores[aleatorio]);
+    }
         //console.log(`Text: ${color}`);
+    }
+    function capturarClick(){
+        if(bloqueo === 0){
+            console.log('vale 0');
+            setBloqueo(1);
+        }else{
+            console.log('vale 1');
+            setBloqueo(0);
+            
+        }
+
     }
     return (
        
         <div  className ={color} onMouseMove={capturarRaton}>
-            Pase el ratón por esta linea
+         <div  className ="bloquea" onMouseDown={capturarClick}></div>
+            pulse la linea  para bloquear /desbloquear
         </div>
     );
 }
